83a63a9394356cd5c1a3c57b37591c3a607b0c15
Eden Brekke <eden.lorrai@gmail.com>
2022-05-12 16:01:29 -0700

    finished up email function

--- automation/automation.py
+++ automation/automation.py
@@ -4,7 +4,6 @@ with open('./automation/potential-contacts.txt') as file:
   text_content = file.read()
   
 
-email_pattern = r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}'
 def get_phone_numbers():
   phone_pattern = r'\b\d{3}\D?\d{3}\D?\d{4}'
   potential_phone_numbers = re.findall(phone_pattern, text_content)
@@ -19,23 +18,24 @@ def get_phone_numbers():
     hyphened_num_list.append(final_num)
 
   without_duplicates_num = list(dict.fromkeys(hyphened_num_list))
-  output_phone = "\n".join(without_duplicates_num)
-  print(output_phone)
+  
+  with open('./automation/phone_numbers.txt', 'w') as file:
+    for number in without_duplicates_num:
+      file.write(number)
+      file.write('\n')
 
-  # with open('./automation/phone_numbers.txt', 'w') as file:
-  #   for number in output_phone:
-  #     file.write(number)
-  #     file.write('\n')
+def get_emails():
+  email_pattern = r'[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}'
+  potential_emails = re.findall(email_pattern, text_content)
+  without_duplicates_email = list(dict.fromkeys(potential_emails))
+  output_email = "\n".join(without_duplicates_email)
 
-potential_emails = re.findall(email_pattern, text_content)
-
-output_email = "\n".join(potential_emails)
-
-print(output_email)
-    
-# with open('./automation/emails.txt', "w") as file:
-#   for email in potential_emails:
-#     file.write(email)
-#     file.write('\n')
+  print(output_email)
+      
+  with open('./automation/emails.txt', "w") as file:
+    for email in potential_emails:
+      file.write(email)
+      file.write('\n')
 
 get_phone_numbers()
+get_emails()
